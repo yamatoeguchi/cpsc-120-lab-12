3436a4010e54eff48362e9272a852669c719a167
Yamato Eguchi <yamatoe1227@csu.fullerton.edu>
2021-05-07 19:12:10 -0700

    this is 1000 spheres

--- part-1/rt.cc
+++ part-1/rt.cc
@@ -1,3 +1,13 @@
+// Yamato Eguchi
+// CPSC 120-01
+// 2021-05-06
+// yamatoe1227@csu.fullerton.edu
+// @yamatoeguchi
+//
+// Lab 12-01
+//
+//This is my many spheres
+//
 
 #include <chrono>
 #include <cmath>
@@ -20,36 +30,25 @@ using namespace std;
 /// \param r A ray eminating from the camera through the scene
 /// \param world The world - the scene that will be rendered
 /// \returns The color visible from that ray
-Color RayColor(const Ray& r, const Sphere& world) {
+Color RayColor(const Ray& r, const std::vector<std::shared_ptr<Hittable>>& world) {
   HitRecord rec;
   Color c;
+  HitRecord tmp_rec;
+  bool hit_anything = false;
+  double t_min = 0.0;
+  double closest_so_far = kInfinity;
+  for (const auto& object : world) {
+    if (object->hit(r, t_min, closest_so_far, tmp_rec)) {
+      hit_anything = true;
+      closest_so_far = tmp_rec.t;
+      rec = tmp_rec;
+    }
+  }
   // check to see if the ray intersects with the world
-  if (world.hit(r, 0, kInfinity, rec)) {
+  if (hit_anything) {
+    c = rec.material->reflect_color(r, rec);
     // This is the base color of the sphere. You may set this color
     // to whatever you like.
-    Color sphere_color{1, 1, 0};
-    const double kAmbientReflection = 0.3;
-    const double kDiffuseReflection = 0.7;
-    const double kSpecularReflection = 0.5;
-    const double kSpecularShininess = 32.0;
-    // If the ray intersects an object in the world (the sphere), then
-    // set the color to be the
-    // c = 0.5 * (rec.normal + sphere);
-    Vec3 light{1, 1, 0.25};
-    Vec3 to_light_vector = UnitVector(light - rec.p);
-    Vec3 unit_normal = UnitVector(rec.normal);
-    Vec3 to_viewer = UnitVector(-rec.p);
-    Vec3 reflection = Reflect(to_light_vector, unit_normal);
-
-    Color phong_ambient = kAmbientReflection * sphere_color;
-    Color phong_diffuse =
-        kDiffuseReflection * std::max(Dot(to_light_vector, unit_normal), 0.0) * sphere_color;
-    Color phong_specular =
-        kSpecularReflection *
-        std::pow(std::max(Dot(reflection, to_viewer), 0.0), kSpecularShininess) * sphere_color;
-
-    Color phong = phong_ambient + phong_diffuse + phong_specular;
-    c = Clamp(phong, 0, 1);
   } else {
     // Color sky_top{.5, .4, 0};
     // Color sky_bottom{.2, .1, .5};
@@ -101,12 +100,14 @@ int main(int argc, char const* argv[]) {
   /// ratio of 36:24 or 1.5.
   const double kAspectRatio = 16.0 / 9.0;
   // Set the image width to 400 pixels
-  const int kImageWidth = 800;
+  const int kImageWidth = 1000;
   // Calculate the height of the image using the width and aspect ratio.
   // Remember to round the number to the closest integer.
   const int kImageHeight = int(lround(kImageWidth / kAspectRatio));
   // Create a new Image object using the file name provided on the
   // command line.
+  const int kSamplesPerPixel = 20;
+
   Image image(argv_one_output_file_name, kImageWidth, kImageHeight);
   if (!image.is_open()) {
     ostringstream message_buffer("Could not open the file ", ios_base::ate);
@@ -120,7 +121,9 @@ int main(int argc, char const* argv[]) {
   cout << "Image: " << image.height() << "x" << image.width() << "\n";
 
   /// World definition in main
-  auto world = Sphere(Point3(0, 0, -1), 0.5);
+  // auto world = OriginalScene();
+  const int kNumberSpheres = 1000;
+  auto world = RandomScene(kNumberSpheres);
 
   /// Camera definition in main
   /// The [viewport](https://en.wikipedia.org/wiki/Viewport) is the
@@ -165,32 +168,41 @@ int main(int argc, char const* argv[]) {
   // be calculated.
   chrono::time_point<chrono::high_resolution_clock> start =
       chrono::high_resolution_clock::now();
+  Color pixel_color;
   for (int row = image.height() - 1; row >= 0; row--) {
     for (int column = 0; column < image.width(); column++) {
-      // u is the distance from the left edge of the image to the right
-      // edge of the image measured as a percentage.
-      // The column divided by the image.width() - 1 is the percentage of
-      // the distance from the left side of the image to the right.
-      // Consider column = 0 then colum / (image.width() - 1) -> 0.0
-      //          column = (image.width() - 1) / 2 then
-      //          colum / (image.width() - 1) -> 0.5
-      //          column = image.width() - 1 then
-      //          colum / (image.width() - 1) -> 1.0
-      // The same is true for v
-      double u = double(column) / double(image.width() - 1);
-      double v = double(row) / double(image.height() - 1);
-      // Create a ray that starts at the camera's center, the origin, and
-      // travels through the pixel center defined by
-      // kLowerLeftCorner + u * kHorizontal + v * kVertical - kOrigin
-      Ray r{kOrigin,
-            kLowerLeftCorner + u * kHorizontal + v * kVertical - kOrigin};
-      // cout << "row: " << row << " col: " << column << " u: " << u << " v: "
-      // << v << "\n" << r << "\n";
-      // Calculate and return the color at the pixel that Ray r
-      // points through.
-      Color pixel_color = RayColor(r, world);
-      // Write the color to the image file.
+      for (int s = 0; s < kSamplesPerPixel; s++) {
+        // u is the distance from the left edge of the image to the right
+        // edge of the image measured as a percentage.
+        // The column divided by the image.width() - 1 is the percentage of
+        // the distance from the left side of the image to the right.
+        // Consider column = 0 then colum / (image.width() - 1) -> 0.0
+        //          column = (image.width() - 1) / 2 then
+        //          colum / (image.width() - 1) -> 0.5
+        //          column = image.width() - 1 then
+        //          colum / (image.width() - 1) -> 1.0
+        // The same is true for v
+        double u = (double(column) + RandomDouble01())/ double(image.width() - 1);
+        double v = (double(row) + RandomDouble01()) / double(image.height() - 1);
+        // Create a ray that starts at the camera's center, the origin, and
+        // travels through the pixel center defined by
+        // kLowerLeftCorner + u * kHorizontal + v * kVertical - kOrigin
+        Ray r{kOrigin, 
+              kLowerLeftCorner + u * kHorizontal + v * kVertical - kOrigin};
+        pixel_color = pixel_color + RayColor(r, world);
+      }
+        // cout << "row: " << row << " col: " << column << " u: " << u << " v: "
+        // << v << "\n" << r << "\n";
+        // Calculate and return the color at the pixel that Ray r
+        // points through.
+        // Write the color to the image file.
+      double scale = 1.0 / double(kSamplesPerPixel);
+      double r = Clamp(std::sqrt(pixel_color.r() * scale), 0.0, 1.0);
+      double g = Clamp(std::sqrt(pixel_color.g() * scale), 0.0, 1.0);
+      double b = Clamp(std::sqrt(pixel_color.b() * scale), 0.0, 1.0);
+      pixel_color = Color(r, g, b);
       image.write(pixel_color);
+
     }
   }
   // Save the ending time
